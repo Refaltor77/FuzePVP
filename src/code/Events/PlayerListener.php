2b7d0ada3f4ed92fdfd38e4084026c58fa1464f2
Refaltor <elysiomartinspro@gmail.com>
2021-09-25 17:18:02 +0200

    add combo lollll

--- src/code/Events/PlayerListener.php
+++ src/code/Events/PlayerListener.php
@@ -10,21 +10,51 @@ use code\Task\PlayerTask;
 use jasonwynn10\ScoreboardAPI\ScoreboardAPI;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\level\sound\AnvilBreakSound;
+use pocketmine\level\sound\BlazeShootSound;
 use pocketmine\network\mcpe\protocol\ContainerClosePacket;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 
 class PlayerListener implements Listener
 {
 
     private $cancel_send;
+    private array $players = [];
+    private array $combo = [];
+    const COLOR =  [
+        TextFormat::RED,
+        TextFormat::BLUE,
+        TextFormat::DARK_PURPLE,
+        TextFormat::LIGHT_PURPLE,
+        TextFormat::YELLOW,
+        TextFormat::AQUA,
+        TextFormat::YELLOW,
+        TextFormat::YELLOW,
+        TextFormat::AQUA,
+        TextFormat::AQUA,
+        TextFormat::RED,
+        TextFormat::BLUE,
+        TextFormat::DARK_PURPLE,
+        TextFormat::LIGHT_PURPLE,
+        TextFormat::YELLOW,
+        TextFormat::YELLOW,
+        TextFormat::YELLOW,
+        TextFormat::AQUA,
+        TextFormat::AQUA,TextFormat::AQUA,
+        TextFormat::YELLOW,
+        TextFormat::AQUA,
+        TextFormat::AQUA
+    ];
 
     public function onKill(PlayerDeathEvent $event){
         $player = $event->getPlayer();
@@ -53,14 +83,6 @@ class PlayerListener implements Listener
         //FuzePVP::getInstance()->getScheduler()->scheduleRepeatingTask(new PlayerTask($player), 20);
     }
 
-    public function onMessage(PlayerChatEvent $event){
-        $player = $event->getPlayer();
-        $message = $event->getMessage();
-
-        $event->setCancelled();
-
-        Server::getInstance()->broadcastMessage("§l§f[§r§e". Kill::getKill($player) ."§l§f][§r§7Novice§f§l]§r§7 " . $player->getName() . "§8:§7 " . $message);
-    }
 
     public function onBreak(BlockBreakEvent $event){
         $player = $event->getPlayer();
@@ -95,5 +117,43 @@ class PlayerListener implements Listener
         }
     }
 
+    public function onDamage(EntityDamageByEntityEvent $event): void {
+        $victim = $event->getEntity();
+        $damager = $event->getDamager();
+        if ($victim instanceof Player && $damager instanceof Player) {
+            if (isset($this->players[$damager->getXuid()])) {
+                if ($this->players[$damager->getXuid()] < time()) {
+                    EconomyAPI::getInstance()->addMoney($damager, 1);
+                    $damager->sendTip('§a+ 1 ');
+                    $this->players[$damager->getXuid()] = time() + 1;
+                }
+            } else {
+                EconomyAPI::getInstance()->addMoney($damager, 1);
+                $damager->sendTip('§a+ 1 ');
+                $this->players[$damager->getXuid()] = time() + 1;
+            }
+
+            if (isset($this->combo[$damager->getXuid()])) {
+                if ($this->combo[$damager->getXuid()][1] > time()) {
+                    $combo = $this->combo[$damager->getXuid()][0];
+                    $color = self::COLOR[array_rand()];
+                    $damager->getLevel()->addSound(new BlazeShootSound(), [$damager]);
+                    $damager->sendPopup($color . " X" . $combo);
+                    $combo++;
+                    $this->combo[$damager->getXuid()] = [$combo, time() + 2];
+                } else {
+                    $color = self::COLOR[array_rand()];
+                    $damager->sendPopup($combo . "COMBO PERDU !");
+                    $damager->getLevel()->addSound(new AnvilBreakSound(), [$damager]);
+                    $this->combo[$damager->getXuid()] = [1, time() + 2];
+                }
+            } else {
+                $color = self::COLOR[array_rand()];
+                $damager->getLevel()->addSound(new BlazeShootSound(), [$damager]);
+                $damager->sendPopup($combo . "COMBOOOOOOOO");
+                $this->combo[$damager->getXuid()] = [1, time() + 2];
+            }
+        }
+    }
 
 }
